f23adb3a2927d976db58589788f012f8b13c3fe9
Gustagols <maltag.tavow@gmail.com>
2025-09-28 13:38:11 -0300

    sidebar fix

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,8 +1,17 @@
 const hamburguerButton = document.querySelector('.hamburguer-icon-container');
 const sidebar = document.querySelector('.sidebar');
+const sidebarLink = document.querySelectorAll('.sidebar-link');
+const sidebarText = document.querySelectorAll('.sidebar-icon-text');
 
 hamburguerButton.addEventListener('click', () => {
     sidebar.classList.toggle('active');
     
-});
+    sidebarLink.forEach(icon => {
+    icon.classList.toggle('active');
+    });
 
+    sidebarText.forEach(text => {
+    text.classList.toggle('active');
+    });
+
+});
